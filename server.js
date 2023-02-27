8d9fb29eca9836043539ce1242eaaa1d8e0447bf
nasanchez7 <nadirblanco02@gmail.com>
2023-02-27 20:58:48 -0300

    configuranod puerto

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const MongoDb = require('./Contenedores/ContenedorMongoDb.js')
-const port = 8080
+const port = process.env.PORT || 8080
 const multer = require('multer')
 
 
